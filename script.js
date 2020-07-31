add3c67b3e85bf3414c8b8c331e19e4d194d38e5
R-Prince <reneprince93@icloud.com>
2020-07-31 14:40:09 +0000

    Add search bar to HTML and retrieve text

--- script.js
+++ script.js
@@ -1,13 +1,42 @@
 var xhr = new XMLHttpRequest();
+var searchText = document.getElementById('searchBar');
+
+document.querySelector('#searchBar').addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        console.log(searchText.value);
+        event.preventDefault()
+    }
+});
+
+var search = function (){
+    console.log(searchText.value);
+    event.preventDefault()
+};
+
+/*searchText.addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+    console.log(searchText.value);
+    }
+    event.preventDefault()
+});*/
 
 xhr.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-        console.log(this.responseText);
+        var obj = JSON.parse(this.responseText);
+        var results = obj.candidates[0];
+        document.getElementById('name').innerHTML = results.name;
+        document.getElementById('address').innerHTML = results.formatted_address;
+        document.getElementById('rating').innerHTML = results.rating;
+        document.getElementById('price').innerHTML = results.price_level;
+
+        console.log(JSON.parse(this.responseText));
     }
 };
 
-xhr.open("GET", "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=wembley&stadium&inputtype=textquery&fields=name,formatted_address&key=AIzaSyCdBqz5aZKn5u3_GeKoUVzRZS6bsw33p_o");
+/* xhr.open("GET", "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Wembley%20Stadium&inputtype=textquery&fields=photos,formatted_address,name,rating,price_level&key=AIzaSyCdBqz5aZKn5u3_GeKoUVzRZS6bsw33p_o");
 
-xhr.send();
+xhr.send(); */
+
+
 
 
